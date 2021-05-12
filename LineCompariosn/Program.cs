a922bd950a42159c6afa4249b77d481beb6cfc5d
unknown <jimpidas>
2021-05-12 18:59:47 +0530

    [Jyosmita]:Added the code two compare the length between two lines

--- LineCompariosn/Program.cs
+++ LineCompariosn/Program.cs
@@ -8,6 +8,38 @@ namespace LineComparisonProblem
         {
             return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
         }
+
+        private static void compareToMethod(double l1, double l2)
+        {
+
+
+            if (l1.CompareTo(l2) == 0)
+            {
+                Console.WriteLine("Line1 is equal to Line2 ");
+            }
+            else if (l1.CompareTo(l2) < 0)
+            {
+                Console.WriteLine("Using compareTo method,Line1  is less than line2 ");
+            }
+            else
+            {
+                Console.WriteLine("Using compareTo method,Line1  is greater than Line2");
+            }
+        }
+
+        private static void equalToMethod(double l1, double l2)
+        {
+            if (l1 == l2)
+                Console.WriteLine("Lines are equal");
+            else
+                Console.WriteLine("Lines are  not equal");
+            if (l1 > l2)
+                Console.WriteLine("Using equalToMethod ,Line1  is greater than Line2  ");
+            else if (l2 > l1)
+                Console.WriteLine("Using equalToMethod,Line1  is less than line2  ");
+        }
+
+
         static void Main(string[] args)
         {
             double line1, line2;
@@ -16,15 +48,14 @@ namespace LineComparisonProblem
             line2 = (Math.Round(length(3, 7, 8, 4) * 100000.0) / 100000.0);
             Console.WriteLine(line1);
             Console.WriteLine(line2);
-            if (line1 == line2)
-                Console.WriteLine("Lines are equal");
-            else
-                Console.WriteLine("Lines are  not equal");
-            if (line1 > line2)
-                Console.WriteLine("Line 1 is greater : " + line1);
-            else if (line2 > line1)
-                Console.WriteLine("Line 2 is greater : " + line2);
+
+            equalToMethod(line1, line2);
+            compareToMethod(line1, line2);
         }
 
     }
 }
+
+
+
+
