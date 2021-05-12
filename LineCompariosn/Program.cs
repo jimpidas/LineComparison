959c969b20294a707507fda47dfd8df0db6d0159
unknown <jimpidas>
2021-05-12 18:48:19 +0530

    [Jyosmita]:Added the code to check equality between two line

--- LineCompariosn/Program.cs
+++ LineCompariosn/Program.cs
@@ -10,9 +10,16 @@ namespace LineComparisonProblem
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("The length of the line is : ");
-            Console.WriteLine(Math.Round(length(5, 6, 7, 3) * 100000.0) / 100000.0);
+            double line1, line2;
 
+            line1 = (Math.Round(length(5, 6, 7, 3) * 100000.0) / 100000.0);
+            line2 = (Math.Round(length(3, 7, 8, 4) * 100000.0) / 100000.0);
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
+            if (line1 == line2)
+                Console.WriteLine("Lines are equal");
+            else
+                Console.WriteLine("Lines are  not equal");
 
 
         }
